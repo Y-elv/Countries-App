f4263971d36913c250039b416ff4833293f78e9e
mugisha <mugishaelvis456@gmail.com>
2023-08-08 12:23:13 +0200

    adding few things

--- src/Home.jsx
+++ src/Home.jsx
@@ -40,7 +40,7 @@ function Home() {
         .then((data) => setData(data), setData2(data))
         .catch((err) => console.log("Error:", err.message));
     }
-  }, [selectInput]);
+  }, [selectInput,data]);
 
   const handleChangeSelect = (e) => {
     setSelectInput(e.target.value);
